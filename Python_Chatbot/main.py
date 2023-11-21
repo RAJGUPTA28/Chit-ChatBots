8b028563d21bb487563a05841b0368f50508052d
Raj Gupta <89084549+RAJGUPTA28@users.noreply.github.com>
2023-11-21 10:58:41 +0530

    Update main.py

--- Python_Chatbot/main.py
+++ Python_Chatbot/main.py
@@ -1,1 +1,55 @@
+import nltk
+nltk.download('punkt')
+nltk.download('wordnet')
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+import json
+import pickle
+
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+from keras.optimizers import SGD
+import random
+
+words=[]
+classes = []
+documents = []
+ignore_words = ['?', '!']
+data_file = open('intents.json').read()
+intents = json.loads(data_file)
+
+
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+
+        #tokenize
+        w = nltk.word_tokenize(pattern)
+        words.extend(w)
+        #add documents in the corpus
+        documents.append((w, intent['tag']))
+
+        # add to our classes list
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+
+
+
+words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
+words = sorted(list(set(words)))
+
+classes = sorted(list(set(classes)))
+
+print (len(documents), "documents")
+print (len(classes), "classes", classes)
+print (len(words), "unique lemmatized words", words)
+
+
+pickle.dump(words,open('words.pkl','wb'))
+pickle.dump(classes,open('classes.pkl','wb'))
+
+
+
 
