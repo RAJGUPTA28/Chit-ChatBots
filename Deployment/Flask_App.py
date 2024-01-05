2a660d0734e2d0cd0906ea5d71f91c08d4d81b07
Raj Gupta <89084549+RAJGUPTA28@users.noreply.github.com>
2024-01-05 12:51:29 +0530

    Update Flask_App.py

--- Deployment/Flask_App.py
+++ Deployment/Flask_App.py
@@ -64,4 +64,20 @@ def chatbot_response(msg):
     res = getResponse(ints, intents)
     return res
 
+from flask import Flask, render_template, request
 
+app = Flask(__name__)
+app.static_folder = 'static'
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    return chatbot_response(userText)
+
+
+if __name__ == "__main__":
+    app.run()
