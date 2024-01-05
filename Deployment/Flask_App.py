c160c0bd31517e0fd6e9c1ae26dabc7d230d8213
Raj Gupta <89084549+RAJGUPTA28@users.noreply.github.com>
2024-01-05 12:51:48 +0530

    Update Flask_App.py

--- Deployment/Flask_App.py
+++ Deployment/Flask_App.py
@@ -50,20 +50,6 @@ def predict_class(sentence, model):
         return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
     return return_list
 
-def getResponse(ints, intents_json):
-    tag = ints[0]['intent']
-    list_of_intents = intents_json['intents']
-    for i in list_of_intents:
-        if(i['tag']== tag):
-            result = random.choice(i['responses'])
-            break
-    return result
-
-def chatbot_response(msg):
-    ints = predict_class(msg, model)
-    res = getResponse(ints, intents)
-    return res
-
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
