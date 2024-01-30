aeeef21c2d20b7ce759dcd6be7f3a7bc8ac3e5c4
Raj Gupta <89084549+RAJGUPTA28@users.noreply.github.com>
2024-01-30 09:43:22 +0530

    Update main.py

--- OpenAI_ChatBot/main.py
+++ OpenAI_ChatBot/main.py
@@ -1,1 +1,17 @@
 
+import openai 
+openai.api_key = 'YOUR_API_KEY'
+messages = [ {"role": "system", "content": 
+			"You are a intelligent assistant."} ] 
+while True: 
+	message = input("User : ") 
+	if message: 
+		messages.append( 
+			{"role": "user", "content": message}, 
+		) 
+		chat = openai.ChatCompletion.create( 
+			model="gpt-3.5-turbo", messages=messages 
+		) 
+	reply = chat.choices[0].message.content 
+	print(f"ChatGPT: {reply}") 
+	messages.append({"role": "assistant", "content": reply}) 
